b5a49289e513fe4edac5b824885c72a59c323efb
afg <afg984@gmail.com>
2016-08-30 12:46:40 +0800

    clang support

--- gmon/mcount.s
+++ gmon/mcount.s
@@ -1,5 +1,5 @@
 .global __gnu_mcount_nc
-.type __gnu_mcount_nc,function
+.type __gnu_mcount_nc, function
 .align  2
 
 __gnu_mcount_nc:
@@ -9,4 +9,19 @@ __gnu_mcount_nc:
     str     r0,[sp, #16]
     str     r1,[sp, #20]
     bl      __mcount_internal
-    pop     {r0-r3,lr,pc}
+    pop     {r0-r3,lr}
+    pop     {pc}
+
+
+.global mcount
+.type mcount, function
+.align  2
+
+mcount:
+    push    {r0-r3,lr}
+    mov     r1,lr
+    ldr     r0,[fp, #4]
+    bl      __mcount_internal
+    pop     {r0-r3}
+    ldr     lr,[fp, #4]
+    pop     {pc}
